f0e089a5f9c55276fc05821742d8f119b8395c7d
valentinbreiz <valentinbreiz@gmail.com>
2023-05-26 21:36:46 +0200

    Add base for container widget

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import '../../styles.css';
 import BlockIpWidget from './widgets/BlockIpWidget';
 import ConfigGen from './widgets/ConfigGenWidget';
 import LogViewerWidget from './widgets/ListConnectionsWidget';
+import ContainerMonitorWidget from './widgets/DockerWidget';
 import { Grid } from '@mui/material';
 
 const Dashboard = () => {
@@ -14,6 +15,9 @@ const Dashboard = () => {
         </Grid>
         <Grid item>
           <BlockIpWidget />
+        </Grid>
+        <Grid item>
+          <ContainerMonitorWidget />
         </Grid>
       </Grid>
       <Grid item style={{ flexGrow: 1, width: '100%' }}>
---
+++ src/components/dashboard/widgets/DockerWidget.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Grid, Paper, Typography, Card, CardContent, Box } from '@mui/material';
+import ErrorIcon from '@mui/icons-material/Error';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+
+const ContainerMonitorWidget = () => {
+  const [containers, setContainers] = useState([]);
+
+  useEffect(() => {
+    fetchContainers();
+  }, []);
+
+  const fetchContainers = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/honeypot/containers');
+      setContainers(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getContainerStatus = (status) => {
+    if (status.startsWith('Up')) {
+      return <CheckCircleIcon color="success" />;
+    } else {
+      return <ErrorIcon color="error" />;
+    }
+  };
+
+  return (
+    <Grid item xs={12}>
+      <Paper sx={{ p: 2, height: '520px', width: '25em', maxWidth: '100%', margin: '1em', overflow: 'hidden' }}>
+        <Typography variant="h6" mb={2}>Honeypot services</Typography>
+        <Box
+          sx={{
+            height: 'calc(100% - 36px)',
+            overflow: 'auto',
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+              alignItems: 'stretch',
+              gap: 2,
+            }}
+          >
+            {containers.map((container, index) => (
+              <Card variant="outlined" key={index}>
+                <CardContent>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'flex-start',
+                    }}
+                  >
+                    <Typography variant="h6">{container.name}</Typography>
+                    {getContainerStatus(container.status)}
+                  </Box>
+                  <Typography variant="body2" color="text.secondary">Status: {container.status}</Typography>
+                  <Typography variant="body2" color="text.secondary">IP: {container.ip.split('/')[0]}</Typography>
+                </CardContent>
+              </Card>
+            ))}
+          </Box>
+        </Box>
+      </Paper>
+    </Grid>
+  );
+};
+
+export default ContainerMonitorWidget;
