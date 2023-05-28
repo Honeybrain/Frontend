c566388c24e6b6966c021a6ad6f7b8ca35989a4d
valentinbreiz <valentinbreiz@gmail.com>
2023-05-28 14:39:32 +0200

    Make widgets longer

--- src/components/dashboard/widgets/BlockIpWidget.jsx
+++ src/components/dashboard/widgets/BlockIpWidget.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Typography, TextField, Button, Paper, Grid, Snackbar, Alert, List, ListItem, ListItemText, ListItemSecondaryAction, IconButton } from '@mui/material';
+import { Typography, TextField, Button, Paper, Grid, Snackbar, Alert, List, ListItem, ListItemText, ListItemSecondaryAction, IconButton, Box } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Divider } from '@mui/material';
 
@@ -59,56 +59,76 @@ const BlacklistPage = () => {
   };
 
   return (
-    <Paper sx={{ p: 2, height: '320px', width: '25em', maxWidth: '100%', margin: '1em', overflow: 'hidden' }}>
-      <Typography variant="h6" mb={2}>Block an IP</Typography>
-      <Grid container spacing={2} direction="column" alignItems="stretch" component="form" onSubmit={handleSubmit}>
-        <Grid item>
-          <TextField
-            variant="outlined"
-            required
-            fullWidth
-            id="ip"
-            label="IP Address"
-            autoFocus
-            value={ip}
-            onChange={(e) => setIp(e.target.value)}
-          />
+    <Grid item xs={12}>
+      <Paper sx={{ p: 2, height: '420px', width: '25em', maxWidth: '100%', margin: '1em', overflow: 'hidden' }}>
+        <Typography variant="h6" mb={2}>Block an IP</Typography>
+        <Grid container spacing={2} direction="column" alignItems="stretch" component="form" onSubmit={handleSubmit}>
+          <Grid item>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              id="ip"
+              label="IP Address"
+              autoFocus
+              value={ip}
+              onChange={(e) => setIp(e.target.value)}
+            />
+          </Grid>
+          <Grid item>
+            <Button type="submit" variant="contained" color="primary">
+              Block IP
+            </Button>
+          </Grid>
+          <Grid item>
+            <Typography variant="h6" component="h2" gutterBottom mt={2}>
+              Currently Blocked IPs
+            </Typography>
+          </Grid>
+          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+              {alertText}
+            </Alert>
+          </Snackbar>
         </Grid>
-        <Grid item>
-          <Button type="submit" variant="contained" color="primary">
-            Block IP
-          </Button>
-        </Grid>
-        <Grid item>
-          <Typography variant="h6" component="h2" gutterBottom mt={2}>
-            Currently Blocked IPs
-          </Typography>
-          <List sx={{ maxHeight: '280px', overflow: 'auto' }}>
-            {blacklistedIPs.map((blacklistedIP, index) => (
-              <ListItem key={index} sx={{
-                my: 1,
-                px: 2,
-                bgcolor: index % 2 === 0 ? 'action.hover' : 'background.default',
-                borderRadius: 1
-              }}>
-                <ListItemText primary={blacklistedIP} />
-                <ListItemSecondaryAction>
-                  <IconButton edge="end" aria-label="delete" onClick={() => handleUnblock(blacklistedIP)}>
-                    <DeleteIcon color="error" />
-                  </IconButton>
-                </ListItemSecondaryAction>
-                {index !== blacklistedIPs.length - 1 && <Divider />}
-              </ListItem>
-            ))}
-          </List>
-        </Grid>
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-          <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-            {alertText}
-          </Alert>
-        </Snackbar>
-      </Grid>
-    </Paper>
+        <Box
+          sx={{
+            height: 'calc(100% - 230px)',
+            overflow: 'auto',
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+              alignItems: 'stretch',
+              gap: 2,
+              marginBottom: 3,  // Ajoutez une marge en bas
+            }}
+          >
+            <List sx={{ overflow: 'auto' }}>
+              {blacklistedIPs.map((blacklistedIP, index) => (
+                <ListItem key={index} sx={{
+                  my: 1,
+                  px: 2,
+                  bgcolor: index % 2 === 0 ? 'action.hover' : 'background.default',
+                  borderRadius: 1
+                }}>
+                  <ListItemText primary={blacklistedIP} />
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete" onClick={() => handleUnblock(blacklistedIP)}>
+                      <DeleteIcon color="error" />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                  {index !== blacklistedIPs.length - 1 && <Divider />}
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Box>
+      </Paper>
+    </Grid>
   );
 }
 
