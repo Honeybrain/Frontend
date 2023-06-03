c713c2b0cf6452417fb0bea6f733bb8b96076b69
Paul LEBOBE <paul.lebobe@epitech.eu>
2023-06-03 11:16:39 +0200

    fixup! fixup! resetPassword

--- src/components/UserParams.jsx
+++ src/components/UserParams.jsx
@@ -3,11 +3,7 @@ import '../styles.css';
 
 const UserParams = () => {
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [state, setState] = useState(0);
     const [submitted, setSubmitted] = useState(true);
-    let statement = null;
-
 
     const handleSubmit = (e) => {
       e.preventDefault();
@@ -27,29 +23,9 @@ const UserParams = () => {
         .catch((error) => {
           console.error(error);
         });
-    }
+      }
   
-    if (state === 1) {
-      statement = (
-        <div className='form-container'>
-          <h2>Changement d'adresse email</h2>
-          <form id="register-form">
-            <div className="input-group">
-              <input
-                onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                name="email"
-                placeholder="New e-mail"
-                required
-              />
-            </div>
-            <button type="submit">Valider</button>
-          </form>
-        </div>
-      );
-    }
-    if (state === 2) {
-      statement = (
+    return (
         <div className='form-container'>
           <h2>Changement de mot de passe</h2>
           {submitted ?
@@ -67,26 +43,10 @@ const UserParams = () => {
             <button type="submit">Réinitialiser le mot de passe</button>
           </form>
           :
-          <div> Vous allez recevoir un email permtettant de réinitialiser votre mot de passe</div>
+          <div> Vous allez recevoir un email permtettant de réinitialiser votre mot de passe, pensez à vérifier votre dossier spam</div>
           }
         </div>
-      );
-    }
-  
-    return (
-      <div className='form-container'>
-        <h2>Paramètres utilisateurs</h2>
-        {state != 1 ?
-        <button type="button" onClick={() => setState(1)}>Changer d'adresse email</button>
-        : null
-        }
-        {state != 2 ?
-        <button type="button" onClick={() => setState(2)}>Changer de mot de passe</button>
-        : null
-        }
-        {statement}
-      </div>
-    );
+    )
   };
   
   export default UserParams;
