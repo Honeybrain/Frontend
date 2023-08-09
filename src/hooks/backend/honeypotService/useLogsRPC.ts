caa4ae47aa224ded1f10320e45864e9aa6467611
valentinbreiz <valentinbreiz@gmail.com>
2023-08-09 03:17:07 +0200

    ✨ Implement connections manager

--- src/hooks/backend/honeypotService/useLogsRPC.ts
+++ src/hooks/backend/honeypotService/useLogsRPC.ts
@@ -1,16 +1,17 @@
 import React from "react";
 import { transport } from "../../../environment";
 import { LogsClient } from '@protos/logs.client';
-import { LogRequest, LogReply } from '@protos/logs';
+import { LogRequest } from '@protos/logs';
 
 const useLogsRPC = () => {
   const client = React.useMemo(() => new LogsClient(transport), []);
   const [logs, setLogs] = React.useState<string | undefined>('');
+  const controller = new AbortController();
 
   const streamLogs = React.useCallback(async () => {
     const request: LogRequest = LogRequest.create();
 
-    const stream = client.streamLogs(request);
+    const stream = client.streamLogs(request, { abort: controller.signal });
     stream.responses.onNext((message) => {
       setLogs(message?.content);
     });
@@ -18,6 +19,10 @@ const useLogsRPC = () => {
 
   React.useEffect(() => {
     streamLogs();
+
+    return () => {
+      controller.abort();
+    }
   }, []);
 
   return {
